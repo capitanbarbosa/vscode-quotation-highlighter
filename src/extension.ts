7038db9a90d7ccd7de7f343e1f85a25ef0b7c327
Wiz <wizbarbosa@gmail.com>
2023-11-22 17:14:32 -0500

    added support for backticks

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,25 @@ export function activate(context: vscode.ExtensionContext) {
 
     const doc = editor.document;
     const selection = editor.selection;
-    const range = doc.getWordRangeAtPosition(selection.active, /["']([^"']*)["']/);
+    // Regex updated to include groups for different quotes
+    const regex = /(`([^`]*)`)|("([^"]*)")|('([^']*)')/;
+    const range = doc.getWordRangeAtPosition(selection.active, regex);
 
     if (range) {
-      // Create a new range that starts right after the opening quote and ends right before the closing quote
+      const matchedText = doc.getText(range);
+
+      // Determine the offset based on the type of quote
+      let startOffset = 0, endOffset = 0;
+      if (matchedText.startsWith("`")) {
+        startOffset = endOffset = 1; // offset for backticks
+      } else if (matchedText.startsWith("\"") || matchedText.startsWith("'")) {
+        startOffset = endOffset = 1; // offset for single or double quotes
+      }
+
+      // Create a new range that adjusts for the type of quote
       const textInsideQuotesRange = new vscode.Range(
-        range.start.translate(0, 1), // Move start position one character to the right
-        range.end.translate(0, -1)   // Move end position one character to the left
+        range.start.translate(0, startOffset),
+        range.end.translate(0, -endOffset)
       );
       
       editor.selection = new vscode.Selection(textInsideQuotesRange.start, textInsideQuotesRange.end);
